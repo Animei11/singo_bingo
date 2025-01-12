5d999e2fac14574ee8f1928acdba3107f4af8b84
Meilyn Massie <86135057+Animei11@users.noreply.github.com>
2025-01-12 21:36:32 +0000

    Test react

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
-import io from 'socket.io-client';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
 
 const socket = io.connect("http://localhost:3001");
 
@@ -8,10 +8,10 @@ function App() {
   // Room State
   const [room, setRoom] = useState("");
 
-  // Message States 
+  // Message States
   const [message, setMessage] = useState("");
   const [messageReceived, setMessageReceived] = useState("");
-  
+
   const joinRoom = () => {
     if (room !== "") {
       socket.emit("join_room", room);
@@ -25,25 +25,41 @@ function App() {
   useEffect(() => {
     socket.on("receive_message", (data) => {
       setMessageReceived(data.message);
-    })
-  }, [socket])
+    });
+  }, [socket]);
 
   return (
-    <div className="App">
-      <input 
-      placeholder="Room Number..." 
-      onChange={(event) => {
-        setRoom(event.target.value);
-      }}/>
+    <>
+      <h1>Hello</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
 
-      <button onClick={joinRoom}> Join Room </button>
-      <input placeholder="Message..." onChange={(event) => {
-        setMessage(event.target.value);
-      }}/> 
-      <button onClick={sendMessage}>Send Message</button>
-      <h1>Message:</h1>
-      { messageReceived }
-    </div>
+    // <div className="App">
+    //   <input
+    //     placeholder="Room Number..."
+    //     onChange={(event) => {
+    //       setRoom(event.target.value);
+    //     }}
+    //   />
+
+    //   <button onClick={joinRoom}> Join Room </button>
+    //   <input
+    //     placeholder="Message..."
+    //     onChange={(event) => {
+    //       setMessage(event.target.value);
+    //     }}
+    //   />
+    //   <button onClick={sendMessage}>Send Message</button>
+    //   <h1>Message:</h1>
+    //   {messageReceived}
+    //   <a href="homeWebsite.js"> Test Page </a>
+    // </div>
   );
 }
 
